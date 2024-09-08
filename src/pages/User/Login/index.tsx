7be269d1285c1a338ce5158cff671cc3a3e203ed
PerccyKing <perccyking@gmail.com>
2024-09-08 12:51:34 +0800

    feat

--- src/pages/User/Login/index.tsx
+++ src/pages/User/Login/index.tsx
@@ -112,7 +112,7 @@ const Login: React.FC = () => {
         <meta property="og:description" content="移动电子眼联盟会员平台"/>
         <meta property="og:site_name" content="移动电子眼联盟"/>
         <meta name="author" content="meea.top"/>
-        <meta name="keywords" content="移动, 电子眼, 联盟, 移动电子眼联盟, 电子眼联盟, 移动电子眼"/>
+        <meta name="keywords" content="移动, 电子眼, 联盟, 移动电子眼联盟, 电子眼联盟, 移动电子眼, meea, meea.top,MEEA,MEEA.top,MEEA.TOP"/>
         <meta name="description" content="移动电子眼联盟会员平台"/>
         <meta name="robots" content="index,follow"/>
         <meta name="baidu-site-verification" content="codeva-anWMKZlMCy"/>
